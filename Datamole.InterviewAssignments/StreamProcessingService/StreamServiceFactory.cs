c0fc07572e28de0eaa7e01f678ad7bfb81faf65f
Kancl-1051 <apelttom@gmail.com>
2018-12-11 19:24:17 +0100

    NEW: Initial commit with Assignment

---
+++ Datamole.InterviewAssignments/StreamProcessingService/StreamServiceFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StreamProcessingService
+{
+    public static class StreamServiceFactory
+    {
+        public static IStreamService CreateService()
+        {
+            // Todo:
+
+            return new TApeltauerStreamService();
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Datamole.InterviewAssignments/StreamProcessingService/TApeltauerStreamService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace StreamProcessingService
+{
+    class TApeltauerStreamService : IStreamService
+    {
+        public double CalculateAverage(IEnumerable<IList<double>> dataStreams)
+        {
+            double dataStreamsSum = 0;
+            int dataStreamsItemsCount = 0;
+            foreach (var stream in dataStreams)
+            {
+                int streamItemsCounter = 0;
+                for (; ; streamItemsCounter++)
+                {
+                    try
+                    {
+                        dataStreamsSum = dataStreamsSum + stream[streamItemsCounter];
+                    }
+                    catch (ArgumentOutOfRangeException e)
+                    {
+                        streamItemsCounter = streamItemsCounter - 1;
+                    }
+
+                    finally
+                    {
+                        dataStreamsItemsCount = dataStreamsItemsCount + streamItemsCounter;
+                    }
+                }
+            }
+            return (dataStreamsSum / dataStreamsItemsCount);
+        }
+
+        public double CalculateAverage<T>(IList<T> data, int parallelismDegree, Func<T, double> valueExtractor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<double> CalculateAverageAsync(IEnumerable<Stream> dataStreams)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<double> JoinAndSort(Stream stream1, Stream stream2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
